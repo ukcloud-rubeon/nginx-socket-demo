19b217fa0398fc8ae959ac9404e90a815dee7031
Eric Williams <ewilliams@ukcloud.com>
2019-01-17 10:50:53 +0000

    Updated NGINX config

--- nginx/nginx-start/init.sh
+++ nginx/nginx-start/init.sh
@@ -3,9 +3,9 @@
 # update the config file with the environment 
 # Our demo sets BACKEND_SERVICE and BACKEND_PORT
 
-CONFIG_FILE_TMPL="${NGINX_CONFIGURATION_PATH}/default.conf.tmpl"
+CONFIG_FILE_TMPL="${NGINX_APP_ROOT}/src/nginx-default-cfg/default.conf.tmpl"
 CONFIG_FILE="${NGINX_CONFIGURATION_PATH}/default.conf"
 
-envsubst <"${CONFIG_FILE_TMPL}" > "${CONFIG_FILE}"
+envsubst '${BACKEND_PORT},${BACKEND_SERVICE}' <"${CONFIG_FILE_TMPL}" > "${CONFIG_FILE}"
 
 
